657905e2f048e76efcf3de6b3f08d9fc5427d50c
James Rosado <jamesros@boldgrid.com>
2020-04-24 16:07:05 -0400

    added server-tester.php

---
+++ server-tester.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Server Tester
+ * Plugin URI: https://gitbub/jamesros161/server-tester
+ * Version: 0.1
+ * Author: BoldGrid <support@boldgrid.com>
+ * Author URI: https://www.boldgrid.com/
+ * Description: Debug tool for testing server issues
+ * Text Domain: server-tester
+ * Domain Path: /languages
+ * License: GPL
+ */
+
+ // Silence is Golden.
